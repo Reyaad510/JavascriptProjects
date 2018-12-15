4ff3c64ca92c1a5067f174a9a55e0309804bcca1
Reyaad Rafeek <reyaadrafeek@Reyaads-MacBook-Pro.local>
2018-12-15 18:43:00 -0500

    Added where audio will be replayed with multiple keystrokes

--- Drumkit/main.js
+++ Drumkit/main.js
@@ -10,6 +10,6 @@
         // use querySelectorAll if looking for many
         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
         if(!audio) return; // stop the function from running all together
-
+        audio.currentTime = 0; // rewind to start
         audio.play();
     });
