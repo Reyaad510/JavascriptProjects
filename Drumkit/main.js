48788b36cff2a4de5a85840379f059ba548a273b
Reyaad Rafeek <reyaadrafeek@Reyaads-MacBook-Pro.local>
2018-12-15 21:10:32 -0500

    Completed drumkit project and added background image

--- Drumkit/main.js
+++ Drumkit/main.js
@@ -5,20 +5,40 @@
 //     this.console.log(e);
 // });
 
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; 
+    audio.currentTime = 0; 
+    audio.play();
+    key.classList.add('playing');     
+}
 
-    window.addEventListener('keydown', function(e) {
-        // use querySelectorAll if looking for many
-        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-        if(!audio) return; // stop the function from running all together
-        audio.currentTime = 0; // rewind to start
-        audio.play();
-        // key.classList.remove('playing');
-        // key.classList.toggle('playing');
-        // Below adds the 'playing' class that is located in our css
-        key.classList.add('playing');
-        
-    });
+function removeTransition(e) {
+if(e.propertyName !== 'transform') return; 
+this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound);
+    
+
+
+// With Notes are below
+/*
+    function playSound(e) {
+            // use querySelectorAll if looking for many
+            const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+            const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+            if(!audio) return; // stop the function from running all together
+            audio.currentTime = 0; // rewind to start
+            audio.play();
+            // key.classList.remove('playing');
+            // key.classList.toggle('playing');
+            // Below adds the 'playing' class that is located in our css
+            key.classList.add('playing');     
+    }
 
     function removeTransition(e) {
         if(e.propertyName !== 'transform') return; // skip if it's not a transform
@@ -27,3 +47,5 @@
 
     const keys = document.querySelectorAll('.key');
     keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+    window.addEventListener('keydown', playSound);
+*/
